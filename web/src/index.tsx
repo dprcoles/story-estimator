cfc507ce32ca95de42c2c48be9d40bc434fec189
Daniel Coles <me@danielcoles.co.uk>
2022-04-18 17:25:44 +0100

    :dizzy:  added framer animations/transitions

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Wrapper from "./components/Wrapper";
+import { AnimatePresence } from "framer-motion";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Wrapper>
-    <App />
+    <AnimatePresence exitBeforeEnter>
+      <App />
+    </AnimatePresence>
   </Wrapper>
 );
 
